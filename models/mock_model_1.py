a169635434a08b06226e4f5e512f46729a821fb1
suffvier <suffvier@MacBook-Air-5.local>
2024-09-22 23:28:00 +0800

    add skin 2 models

--- models/mock_model_1.py
+++ models/mock_model_1.py
@@ -8,12 +8,15 @@ from datetime import datetime
 from typing import Hashable, Optional
 
 from models.base_model import *
-
+from .pimple_detection import PimpleDetection
 
 class MockModel1(BaseModel):
-    name = "Model1"
+    name = "PimpleDetection"
 
+    ckpt_path = "shape_predictor_81_face_landmarks.dat"
+    pimple_detector = PimpleDetection(ckpt_path)
     def start(self, sid: Hashable, timestamp: int, *args, **kwargs) -> None:
+        print("Timestamp: ", timestamp)
         print(
             f"111 {self.name} started at {datetime.fromtimestamp(timestamp/1000)} with sid {sid}"
         )
@@ -28,6 +31,29 @@ class MockModel1(BaseModel):
             "HR": "1",
         }
 
+    # def frame(
+    #     self, sid: Hashable, frame: np.ndarray, timestamp: int, *args, **kwargs
+    # ) -> Optional[dict]:
+    #     print(
+    #         f"111 {self.name} start processing sid({sid})'s frame@{datetime.fromtimestamp(timestamp/1000)}"
+    #     )
+
+    #     sleep_time = 0.3  # random.uniform(0.5, 2)
+    #     time.sleep(sleep_time)
+    #     not_none = random.choice((None, True, True, True, True))
+
+    #     print(
+    #         f"111 {self.name} finish processing sid({sid})'s frame@{datetime.fromtimestamp(timestamp/1000)}"
+    #     )
+
+    #     # Example: return a value when finishing a certain frame
+    #     return not_none and {
+    #         "HR": "1",
+    #         "sid": sid,
+    #         "HR_resp_ts": time.time(),
+    #         "HR_process_time": sleep_time,
+    #     }
+
     def frame(
         self, sid: Hashable, frame: np.ndarray, timestamp: int, *args, **kwargs
     ) -> Optional[dict]:
@@ -35,18 +61,23 @@ class MockModel1(BaseModel):
             f"111 {self.name} start processing sid({sid})'s frame@{datetime.fromtimestamp(timestamp/1000)}"
         )
 
-        sleep_time = 0.3  # random.uniform(0.5, 2)
+        sleep_time = 1  # random.uniform(0.5, 2)
         time.sleep(sleep_time)
-        not_none = random.choice((None, True, True, True, True))
-
+        # Demonstrate the usage of helper functions/classes in another file.
+        pimple_num, pimple_bboxes = self.pimple_detector.run(frame)
+        
+        result = {
+            "pimple_num": pimple_num,
+            "pimple_bboxes": pimple_bboxes
+        }
+        
         print(
             f"111 {self.name} finish processing sid({sid})'s frame@{datetime.fromtimestamp(timestamp/1000)}"
         )
-
-        # Example: return a value when finishing a certain frame
-        return not_none and {
-            "HR": "1",
+        
+        return {
+            **result,
             "sid": sid,
-            "HR_resp_ts": time.time(),
-            "HR_process_time": sleep_time,
+            "pimple_resp_ts": time.time(),
+            "pimple_process_time": sleep_time,
         }
--- models/mock_model_2/__init__.py
+++ models/mock_model_2/__init__.py
@@ -8,14 +8,19 @@ import time
 from datetime import datetime
 from typing import Hashable, Optional
 
+from numpy import ndarray
+
 from models.base_model import *
 
 from . import lib
-
+from .eye_bag_detection import EyeBagDetection
 
 class MockModel2(BaseModel):
-    name = "Model2"
+    name = "EyeBagDetection"
 
+    ckpt_path = "shape_predictor_81_face_landmarks.dat"
+    eye_bag_detector = EyeBagDetection(ckpt_path)
+    
     def start(self, sid: Hashable, timestamp: int, *args, **kwargs) -> None:
         print(
             f"222 {self.name} started at {datetime.fromtimestamp(timestamp/1000)} with sid {sid}"
@@ -27,6 +32,28 @@ class MockModel2(BaseModel):
         )
         return lib.get_result()
 
+    # def frame(
+    #     self, sid: Hashable, frame: np.ndarray, timestamp: int, *args, **kwargs
+    # ) -> Optional[dict]:
+    #     print(
+    #         f"222 {self.name} start processing sid({sid})'s frame@{datetime.fromtimestamp(timestamp/1000)}"
+    #     )
+
+    #     sleep_time = 1  # random.uniform(0.5, 2)
+    #     time.sleep(sleep_time)
+    #     # Demonstrate the usage of helper functions/classes in another file.
+    #     a = lib.get_result()
+
+    #     print(
+    #         f"222 {self.name} finish processing sid({sid})'s frame@{datetime.fromtimestamp(timestamp/1000)}"
+    #     )
+    #     return {
+    #         **a,
+    #         "sid": sid,
+    #         "fatigue_resp_ts": time.time(),
+    #         "fatigue_process_time": sleep_time,
+    #     }
+
     def frame(
         self, sid: Hashable, frame: np.ndarray, timestamp: int, *args, **kwargs
     ) -> Optional[dict]:
@@ -37,14 +64,20 @@ class MockModel2(BaseModel):
         sleep_time = 1  # random.uniform(0.5, 2)
         time.sleep(sleep_time)
         # Demonstrate the usage of helper functions/classes in another file.
-        a = lib.get_result()
-
+        has_left_eye_bag, has_right_eye_bag = self.eye_bag_detector.run(frame)
+        
+        result = {
+            "has_left_eye_bag": has_left_eye_bag,
+            "has_right_eye_bag": has_right_eye_bag,
+        }
+        
         print(
             f"222 {self.name} finish processing sid({sid})'s frame@{datetime.fromtimestamp(timestamp/1000)}"
         )
+        
         return {
-            **a,
+            **result,
             "sid": sid,
-            "fatigue_resp_ts": time.time(),
-            "fatigue_process_time": sleep_time,
+            "eye_bag_resp_ts": time.time(),
+            "eye_bag_process_time": sleep_time,
         }
