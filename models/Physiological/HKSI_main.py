74865979e50cd19f04928f670e92b720663ae1d5
Ismoil Odinaev <87357271+odismoil@users.noreply.github.com>
2024-11-05 08:28:04 +0800

    Add files via upload

--- models/Physiological/HKSI_main.py
+++ models/Physiological/HKSI_main.py
@@ -6,11 +6,12 @@ HKUST 2023-
 """
 import cv2
 import numpy as np
-from HKSI_physiological_functions import POS, band_pass_filter, Signal_Postprocessing, Face_Algorithm, rgb_mean, HRV
-from HKSI_SkinDetect import SkinDetect
-import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
+from FaceAnalysis import model_FaceAnalysis
+from HeartRate import model_HR
+from HeartRateVariability import model_HRV
+
 
 
 video_path = "D:/Data_Ismoil/Experimental_data/P1_S1.avi"
@@ -20,17 +21,13 @@ video_path = "D:/Data_Ismoil/Experimental_data/P1_S1.avi"
 
 cap = cv2.VideoCapture(video_path)
 fs = cap.get(cv2.CAP_PROP_FPS)
-num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-meanRGB = np.zeros([num_frames-90, 3])
-
-
-sd = SkinDetect(strength=0.2)
 count = 0
+FA = model_FaceAnalysis() #initialize time-series data
+
+
 while True:
-    # Capture the next frame
     ret, frame = cap.read()
 
-    # Check if the frame was read successfully
     if not ret:
         print("End of video reached.")
         break
@@ -38,23 +35,30 @@ while True:
 
     #cv2.imshow('Video Frame', frame)
     
-    if count >= 90:
-        _, skinFace = Face_Algorithm(frame, sd, count-90)
-        b, g, r = rgb_mean(skinFace)
-        meanRGB[count-90, 0], meanRGB[count-90, 1], meanRGB[count-90, 2] = r,g,b 
+    # generate time-series data
+    meanRGB = FA.DetectSkin(frame, fs)
     
+    
+    # calculate instantaneous heart rate
+    ### Note heart rate can be computed after 10 sec of video + 2 sec for stabilization
+    ### evaluting heart rate each second is sufficient. No need for every frame
+    if len(meanRGB) > 12 * fs and len(meanRGB) % fs == 0:
+        model1 = model_HR(meanRGB, fs)
+        hr, _ = model1.evaluate_HR()
+        print(f'heart rate: {np.round(hr,1)} bpm')
+        
+        
     count += 1
-    # Press 'q' to quit
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
-# Release the video capture and close all windows
 cap.release()
 cv2.destroyAllWindows()
 
 
 
 ############ Part 2: Analysis
+######## read time-stamps for interpolation
 
 timestamp_path = "D:/Data_Ismoil/Experimental_data/P1_S1_frames.csv"
 times = np.array(pd.read_csv(timestamp_path))
@@ -64,8 +68,19 @@ for i in range(len(times)):
     if i>0:
         timeInfo[i] = (times[i, 1] - times[0, 1]).total_seconds()
 
-gt_path = "D:/Data_Ismoil/Experimental_data/GT/P1_S1.txt"
 
+############ Evaluate Heart Rate and Heart Rate Variability AFTER video recording finished
+model2 = model_HRV(meanRGB, fs, interpolate=True, time_data=timeInfo, interp_freq=120)
+hrv_dict = model2.HRV_measure()
+print(f'heart rate variability: {hrv_dict["HRV_SDNN"]} ms')
+print(f'heart rate: {hrv_dict["HR"]} bpm')
+
+
+
+'''
+########## import ground truth data
+
+gt_path = "D:/Data_Ismoil/Experimental_data/GT/P1_S1.txt"
 HR_gt = []
 ibi_gt = []
 i = 0
@@ -76,16 +91,7 @@ with open(gt_path, 'r') as text_file:
             HR_gt.append(np.float64(line.split(" ")[0]))
     
         i += 1
-file_path = "D:/Data_Ismoil/Experimental_data/P1_S1.avi.npz"
-file = np.load(file_path, allow_pickle=True)
-dictionary = file['arr_0'].item()
-meanRGB2 = dictionary['meanRGB']
+'''
 
 
-rPPG_cand = POS(meanRGB2, fs)
-rPPG, det = band_pass_filter(rPPG_cand, 5, fs = fs, need_details=True)
 
-SignalAnalysis = Signal_Postprocessing(rPPG_cand, fs, interpolate=True, interp_freq=120, time_data = timeInfo[60:])
-rppg2, ibi = SignalAnalysis.HRV_measure(band=0.2)
-hrv = HRV(ibi)
-SDNN = hrv.sdnn()
--- models/Physiological/HeartRate.py
+++ models/Physiological/HeartRate.py
@@ -13,7 +13,10 @@ class model_HR:
     def __init__(self, meanRGB, fs):
         self.meanRGB = np.array(meanRGB)
         self.fs = fs
-        self.rPPG_cand = POS(self.meanRGB, self.fs)
+        if len(self.meanRGB) == 0:
+            self.rPPG_cand = meanRGB
+        else:
+            self.rPPG_cand = POS(self.meanRGB, self.fs)
     
     def evaluate_HR(self, order = 5, min_freq=0.6, max_freq=4, apply_hanning=False):
         '''This function applies bandpass filter and returns filtered signal. Reccommended values are useful for Heart Rate.
@@ -34,42 +37,45 @@ class model_HR:
             fft_details = (dictionary) FFT details such as FFT freqs, amplitudes and maximum freq in FFT range (only returned if need_details = True)
 
         '''
-
-        # Step 1:  Bandpass filter
         
+
         signal = self.rPPG_cand
         fs = self.fs
-        
-        
-        sos = butter(order, [min_freq, max_freq],
-                     btype='bandpass', output='sos', fs=fs)
-        rPPG = sosfiltfilt(sos, np.double(signal))
-
-
-        if apply_hanning:
-            hanning = np.hanning(len(rPPG))
-            rPPG = hanning * rPPG
-
         fft_details = {}
-        N = len(rPPG)
-        T = 1 / fs
-
-        yf = fft(rPPG)
-
-        freqs = fftfreq(N, T)[:N//2]  # get real valuesof freqs
-
-        yf = 2.0/N * np.abs(yf[0:N//2])  # get real parts of amplitudes
-
-        max_idx = np.where(yf == np.amax(yf))
-        highest_freq = float(freqs[max_idx])
-        HeartRate = highest_freq*60
-
-        fft_details['frequencies'] = freqs
-        fft_details['amplitudes'] = yf
-        fft_details['rPPG'] = rPPG
-
-        return HeartRate, fft_details
+        HeartRate = 0
+        
+        if len(signal) > 10 * fs:
+            
+            sos = butter(order, [min_freq, max_freq],
+                         btype='bandpass', output='sos', fs=fs)
+            rPPG = sosfiltfilt(sos, np.double(signal))
     
+    
+            if apply_hanning:
+                hanning = np.hanning(len(rPPG))
+                rPPG = hanning * rPPG
+    
+            
+            N = len(rPPG)
+            T = 1 / fs
+    
+            yf = fft(rPPG)
+    
+            freqs = fftfreq(N, T)[:N//2]  # get real valuesof freqs
+    
+            yf = 2.0/N * np.abs(yf[0:N//2])  # get real parts of amplitudes
+    
+            max_idx = np.where(yf == np.amax(yf))
+            highest_freq = float(freqs[max_idx])
+            HeartRate = highest_freq*60
+    
+            fft_details['frequencies'] = freqs
+            fft_details['amplitudes'] = yf
+            fft_details['rPPG'] = rPPG
+    
+        
+        return HeartRate, fft_details
+        
     
     
     
